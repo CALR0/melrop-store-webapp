036a93fb4569c1adef316f1cc6266c62ae38e417
Carlos Lizarazo <carlosandr3zlizarazoromero@gmail.com>
2025-06-12 19:19:37 -0500

    Update number in products, hero section

--- src/constants/hero.ts
+++ src/constants/hero.ts
@@ -1,7 +1,7 @@
 import { Stat } from '../types';
 
 export const HERO_STATS: Stat[] = [
-  { number: '10+', label: 'Productos' },
+  { number: '15+', label: 'Productos' },
   { number: '100+', label: 'Clientes satisfechos' },
   { number: '95%', label: 'Tasa de satisfacción' }
 ];
